4602dbf51abf55cac536493755cda91293b08d7a
Tafsirchy <tafsirchy1000@gmail.com>
2026-01-07 23:44:25 +0600

    problem 5

---
+++ problem6.js
@@ -0,0 +1,22 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+function sumArray(arr) {
+  let sum = 0;
+  for (let num of arr) {
+    sum += num;
+  }
+  return sum;
+}
+
+rl.question('Enter a list of numbers separated by spaces: ', (input) => {
+    const numArray = input.split(' ').map(Number);
+    const total = sumArray(numArray);
+    console.log(`The sum of the array is: ${total}`);
+    rl.close();
+    
+})
