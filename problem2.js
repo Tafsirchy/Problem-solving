91da98e1b50edf0ef2cd3f85a5d5857588cc7924
Tafsirchy <tafsirchy1000@gmail.com>
2026-01-07 22:38:31 +0600

    problem 2

---
+++ problem2.js
@@ -0,0 +1,22 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+function checkvowel(char) {
+    const vowels = 'aeiouAEIOU';
+    return vowels.includes(char);   
+}
+
+rl.question('Enter a string: ', (answer) => {
+    let count = 0;
+    for (let i = 0; i < answer.length; i++) {
+        if (checkvowel(answer[i])) {
+            count++;
+        }
+    }
+    console.log(`Number of vowels in the string: ${count}`);
+    rl.close();
+})
